f8970afa4e7310a81ccd5b870a09ba4be28f9f1b
AEGIS 001 <a-gis-001@users.noreply.github.com>
2024-03-06 21:48:19 -0500

    Move rename->move

--- source/A_GIS/Code/Unit/move/__init__.py
+++ source/A_GIS/Code/Unit/move/__init__.py
@@ -1,4 +1,4 @@
-def rename(
+def move(
     *,
     root: type["pathlib.Path"],
     old: type["pathlib.Path"],
--- tree.sh
+++ tree.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-tree -I *.pyc -I __init__.py -I __pycache__ -f -I tests source/A_GIS
+tree -I *.pyc -I __init__.py -I __pycache__ -f -I tests -I __main__.py source/A_GIS
 
 
