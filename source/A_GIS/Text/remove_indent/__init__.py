791c9d622f3630f58e83029c26fc9833c7cc524e
AEGIS 001 <a-gis-001@users.noreply.github.com>
2024-03-10 15:48:55 -0400

    Add docstring

--- source/A_GIS/Text/remove_indent/__init__.py
+++ source/A_GIS/Text/remove_indent/__init__.py
@@ -1,9 +1,25 @@
-def remove_indent(text):
-    lines = text.splitlines()
-    # Find the indentation of the first line
-    min_indent = get_indent(lines[0])
+def remove_indent(*, text:str):
+    """Remove leading whitespace indentation from a block of text
 
-    # Deindent lines, skip lines that would be truncated
+    This function takes a multi-line string as input and removes any common leading whitespace
+    from each line of the text, effectively de-indenting it. It will skip empty lines but
+    as soon as a line with less indentation is found, it will return.
+
+    Args:
+        text (str): A multiline string to be de-indented.
+
+    Returns:
+        str: The input string, but with any common leading whitespace removed from each line.
+    """
+
+    import A_GIS.Text.get_indent
+
+    # Find the indentation of the first line
+    lines = text.splitlines()
+    min_indent = A_GIS.Text.get_indent(lines[0])
+
+    # Deindent lines, stop on the first line that would be truncated and return the
+    # potentially shorter string.
     deindented_lines = []
     for line in lines:
         if not line.strip():  # Keep empty lines
