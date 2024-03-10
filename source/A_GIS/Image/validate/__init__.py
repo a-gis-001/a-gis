29c139436b3f7885532167ff3839522192a46519
AEGIS 001 <a-gis-001@users.noreply.github.com>
2024-03-10 18:10:41 -0400

    Update docstring

--- source/A_GIS/Image/validate/__init__.py
+++ source/A_GIS/Image/validate/__init__.py
@@ -1,6 +1,19 @@
-import PIL.Image
+def validate(image: type['PIL.Image.Image']):
+    """Checks the provided image for A_GIS standards
 
-def validate(image: PIL.Image.Image):
+    This function checks whether an image object has any associated metadata.
+    If there's no metadata, a `ValueError` is raised with a custom message.
+
+    Args:
+        image (PIL.Image.Image): The image to be validated.
+
+    Raises:
+        ValueError: If the provided image does not have any metadata attached.
+
+    Returns:
+        None
+    """
+
     if not hasattr(image, "info") or image.info is None:
         raise ValueError("image must have metadata!")
     pass
