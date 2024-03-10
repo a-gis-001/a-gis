f6bc33914f199a8c4b98c3a9336fef6145474a9b
AEGIS 001 <a-gis-001@users.noreply.github.com>
2024-03-10 18:44:18 -0400

    Update read/write

--- source/A_GIS/File/write/__init__.py
+++ source/A_GIS/File/write/__init__.py
@@ -1,6 +1,21 @@
-import pathlib
+def write(*, content, file: type['pathlib.Path'], binary: bool = False):
+    """Writes content to a file.
 
-def write(*, content, file: pathlib.Path, binary: bool = False):
+	This function writes the provided `content` to a specified `file` using Python's built-in
+	`open()` function, which supports both text and binary writing based on the `binary` flag.
+
+	Args:
+		content (str or bytes): The content to write to the file. If `binary` is True, this should be a bytes object.
+		file (pathlib.Path): A Path object representing the path of the file to write to.
+		binary (bool, optional): If True, the file will be opened in binary mode for writing bytes. Defaults to False.
+
+	Raises:
+		None
+
+	Returns:
+		None
+    """
+
     mode = "w"
     if binary:
         mode += "b"
