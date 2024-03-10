ecc2e60b0ea674492dd7b2942dc4bbf138af7cd5
AEGIS 001 <a-gis-001@users.noreply.github.com>
2024-03-10 18:40:14 -0400

    Update

--- source/A_GIS/Log/init/__init__.py
+++ source/A_GIS/Log/init/__init__.py
@@ -3,20 +3,20 @@ def init(
     filename="app.log",
     format="%(asctime)s %(name)s - %(levelname)s - %(message)s",
 ):
-    """Initializes a logger object with the given filename and log format.
+    """Initializes a logging object with a specified filename and log format.
 
-	This function initializes a logging object using the specified file name and
-	log message format. It returns an instance of the Log class from A_GIS.Log._Log module.
+    This function initializes a Logger instance using the provided file name and log format string.
+    It returns an instance of the Log class from A_GIS.Log._Log module.
 
-	Args:
-		filename (str, optional): The name of the file where logs should be written. Defaults to "app.log".
-		format (str, optional): The format string for log messages. Defaults to "%(asctime)s %(name)s - %(levelname)s - %(message)s".
+    Args:
+        filename (str, optional): The name of the file where logs should be written. Defaults to "app.log".
+        format (str, optional): The format string for log messages. Defaults to "%(asctime)s %(name)s - %(levelname)s - %(message)s".
 
-	Raises:
-		None
+    Raises:
+        None
 
-	Returns:
-		Log: An instance of the Log class representing the created logger object.
+    Returns:
+        Log: An instance of the Log class representing the created logger object.
     """
 
     import A_GIS.Log._Log
