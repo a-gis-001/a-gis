06b7f0b0c1091ad048481c45f3a2f4aa5efcffb5
AEGIS 001 <a-gis-001@users.noreply.github.com>
2024-03-11 08:14:33 -0400

    Add docstring

--- source/A_GIS/Code/guess_name/__init__.py
+++ source/A_GIS/Code/guess_name/__init__.py
@@ -1,4 +1,22 @@
 def guess_name(*, path: type["pathlib.Path"]):
+    """Guesses a package name for a given path based on its parent directory structure.
+
+    The function attempts to find the root directory corresponding to the provided path,
+    then calculates a package name by joining all parts of the relative path from the root's
+    parent directory down to the provided path. If the provided path is a file, it will be
+    used as the base for the calculation and its parent directory will be used instead.
+
+    Args:
+        path (pathlib.Path): The path to guess the package name from.
+
+    Raises:
+        ValueError: If the root corresponding to the provided path could not be found.
+
+    Returns:
+        str: A package name string created by joining parts of the relative path from the
+             root's parent directory down to the provided path, separated by dots.
+    """
+
     import A_GIS.Code.find_root
     import pathlib
 
--- source/A_GIS/Code/is_class/__init__.py
+++ source/A_GIS/Code/is_class/__init__.py
@@ -1,4 +1,19 @@
 def is_class(*, code: str):
+    """Checks if a given Python code string contains a class definition but not a function definition.
+
+    The function uses regular expressions to search for the presence of 'class' at the start of a line (not preceded by any whitespace),
+    indicating that it is likely a class definition, and no instances of 'def', which would indicate a function definition.
+
+    Args:
+        code (str): A string containing Python code to be checked.
+
+    Raises:
+        None
+
+    Returns:
+        bool: True if the provided code contains a class definition but not a function definition, otherwise False.
+    """
+
     # Matches `class` at the start of a line, with no leading whitespace
     import re
 
