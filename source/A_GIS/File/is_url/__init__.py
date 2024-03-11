d73e8f257fd59e6e49e8522e3d8b5a33b7673c35
AEGIS 001 <a-gis-001@users.noreply.github.com>
2024-03-10 22:28:59 -0400

    Update docstrings

--- source/A_GIS/File/is_url/__init__.py
+++ source/A_GIS/File/is_url/__init__.py
@@ -1,4 +1,22 @@
 def is_url(path):
+    """Checks if a given path is a URL with a recognized protocol scheme.
+
+    This function uses the `urlparse` method from the `urllib.parse` module to parse
+    the input path into its components, and checks if it has a recognized protocol
+    scheme (http, https, ftp or ftps). If both conditions are met, the function
+    returns True; otherwise, False.
+
+    Args:
+        path (str): The path to be checked.
+
+    Raises:
+        None
+
+    Returns:
+        bool: True if the path is a URL with a recognized protocol scheme,
+              False otherwise.
+    """
+
     from urllib.parse import urlparse
 
     parsed_path = urlparse(str(path))
--- source/A_GIS/File/read/__init__.py
+++ source/A_GIS/File/read/__init__.py
@@ -1,16 +1,23 @@
 def read(*, file: type["pathlib.Path"], binary: bool = False):
-    """Reads the content of a file. The function opens and reads the content from the specified file path. It can read text or binary files based on the `binary` flag. If `binary` is True, the file is opened in binary mode (i.e., 'rb'). Otherwise, it's opened in text mode (i.e., 'r').
+    """Reads the content of a file
+
+    The function opens and reads the content from the specified file path.
+    It can read text or binary files based on the `binary` flag. If `binary`
+    is True, the file is opened in binary mode (i.e., 'rb'). Otherwise, it's
+    opened in text mode (i.e., 'r').
 
     Args:
         file (pathlib.Path): The path to the file that should be read.
-        binary (bool, optional): If True, the file is read in binary mode. Defaults to False.
+        binary (bool, optional): If True, the file is read in binary mode.
+                                 Defaults to False.
 
     Raises:
         FileNotFoundError: If the specified `file` does not exist.
         IOError: If there's an issue opening or reading from the file.
 
     Returns:
-        str | bytes: The content of the file as a string (if `binary` is False) or bytes object (if `binary` is True).
+        str | bytes: The content of the file as a string (if `binary` is False) or
+                     bytes object (if `binary` is True).
     """
 
     mode = "r"
