18325b36078ee05187b58c634aec9023e3e91fd8
AEGIS 001 <a-gis-001@users.noreply.github.com>
2024-03-24 02:50:00 -0400

    Use docstring_parser

--- source/A_GIS/Code/Docstring/reformat/__init__.py
+++ source/A_GIS/Code/Docstring/reformat/__init__.py
@@ -1,4 +1,4 @@
-def reformat(docstring: str) -> str:
+def reformat(*, docstring: str, width: int = 72) -> str:
     """Reformat a docstring according to PEP 257.
 
     - Split out the first sentence and show as description.
@@ -10,41 +10,45 @@ def reformat(docstring: str) -> str:
     import A_GIS.Text.replace_block
     import A_GIS.Text.insert_block_placeholders
     import A_GIS.Text.reconstitute_blocks
+    import docstring_parser
 
-    # Split the first sentence
-    first_sentence, remaining_text = A_GIS.Text.split_first_sentence(
-        text=docstring
-    )
-    first_sentence = first_sentence.strip()
-    remaining_text = textwrap.dedent(remaining_text).strip()
+    # Parse the docstring
+    parsed = docstring_parser.parse(docstring)
 
-    # Add placeholders.
-    subs, ntext = A_GIS.Text.insert_block_placeholders(
-        text=remaining_text, block_name=r"\S*"
+    # Add placeholders inside blocks inside description for wrapping.
+    subs, wrapped_and_indented_text = A_GIS.Text.insert_block_placeholders(
+        text=parsed.long_description, block_name=r"\S*"
     )
 
-    # Process each paragraph separately to preserve paragraph breaks
-    paragraphs = ntext.split("\n\n")
+    # Wrap parameter descriptions.
+    for i in range(len(parsed.params)):
+        parsed.params[i].description = (
+            textwrap.fill(parsed.params[i].description, width) + "\n"
+        )
+
+    #     # Process each paragraph separately to preserve paragraph breaks
+    paragraphs = wrapped_and_indented_text.split("\n\n")
     wrapped_paragraphs = []
     for paragraph in paragraphs:
-        wrapped_paragraph = textwrap.fill(textwrap.dedent(paragraph), width=68)
+        wrapped_paragraph = textwrap.fill(
+            textwrap.dedent(paragraph), width=width - 4
+        )
         wrapped_paragraphs.append(wrapped_paragraph)
 
     # Join processed paragraphs with double newline and indent
     wrapped_and_indented_text = "\n\n".join(wrapped_paragraphs)
-    wrapped_and_indented_text = textwrap.indent(
-        wrapped_and_indented_text, "    "
-    )
 
     # Reinsert code blocks into their original positions
-    rtext = A_GIS.Text.reconstitute_blocks(
+    parsed.long_description = A_GIS.Text.reconstitute_blocks(
         text=wrapped_and_indented_text, subs=subs
     )
 
-    # Construct final docstring
-    if rtext.strip():
-        docstring = f"{first_sentence}\n\n{rtext}\n    "
-    else:
-        docstring = first_sentence
-
+    # Construct and return final docstring.
+    docstring = docstring_parser.compose(
+        parsed,
+        style=docstring_parser.DocstringStyle.GOOGLE,
+        rendering_style=docstring_parser.RenderingStyle.EXPANDED,
+    )
+    docstring = textwrap.indent(docstring, "    ")
+    docstring = docstring.lstrip()
     return docstring
--- source/A_GIS/Text/insert_block_placeholders/__init__.py
+++ source/A_GIS/Text/insert_block_placeholders/__init__.py
@@ -5,31 +5,34 @@ def insert_block_placeholders(
     block_name: str = "",
     opening: str = "```",
     closing: str = "```",
+    max_blocks: int = 10000,
 ):
     """Inserts placeholders for blocks of specified type in a given text.
 
-    This function scans the input `text` for all blocks of specified type (default is 'python')
-    and replaces each block with a unique placeholder string (default is '__PLACEHOLDER__'). The
-    original blocks are stored in a dictionary, where keys are placeholders and values are the
-    corresponding original blocks.
+    This function scans the input `text` for all blocks of specified
+    type (default is 'python') and replaces each block with a unique
+    placeholder string (default is '__PLACEHOLDER__'). The original
+    blocks are stored in a dictionary, where keys are placeholders and
+    values are the corresponding original blocks.
 
     Args:
-        text (str): Input text to process.
-        label (str, optional): Label for placeholder strings. Defaults to '__PLACEHOLDER__'.
-        block_name (str, optional): Type of block to replace. Defaults to 'python'.
-        opening (str, optional): Opening string denoting the start of a block.
-                                 Defaults to '```'.
-        closing (str, optional): Closing string denoting the end of a block.
-                                Defaults to '```'.
-
-    Raises:
-        None
+        text (str):
+            Input text to process.
+        label (str, optional):
+            Label for placeholder strings. Defaults to '__PLACEHOLDER__'.
+        block_name (str, optional):
+            Type of block to replace. Defaults to empty.
+        opening (str, optional):
+            Opening string denoting the start of a block. Defaults to '```'.
+        closing (str, optional):
+            Closing string denoting the end of a block. Defaults to '```'.
 
     Returns:
-        tuple: A tuple containing two elements:
-            - subs (dict): Dictionary where keys are placeholders and values are original blocks.
-            - text (str): Modified input `text` with placeholders in place of the original blocks.
-    """
+        tuple:
+            A tuple containing two elements:
+            - subs (dict): A dictionary where keys are placeholders and values are the original blocks.
+            - text (str): The modified input `text` with placeholders replacing the original blocks.
+"""
 
     import A_GIS.Text.extract_markdown
     import A_GIS.Text.replace_block
@@ -37,7 +40,7 @@ def insert_block_placeholders(
     block = " "
     subs = {}
     i = 0
-    while block != "" and i < 10000:
+    while block != "" and i < max_blocks:
         block = A_GIS.Text.extract_markdown(
             text=text,
             content_only=False,
