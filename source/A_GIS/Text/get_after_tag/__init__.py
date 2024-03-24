2467bc15bea8e901e18e581934958c30beb9c24a
AEGIS 001 <a-gis-001@users.noreply.github.com>
2024-03-24 17:51:55 -0400

    Update docstrings

--- source/A_GIS/Text/get_after_tag/__init__.py
+++ source/A_GIS/Text/get_after_tag/__init__.py
@@ -1,4 +1,23 @@
 def get_after_tag(*, text: str, tag: str):
+    """Extract string after specified tag.
+
+    This function finds the first occurrence of the given `tag` in the input `text` and returns
+    everything following it, including the tag itself. If the tag is not found in the text,
+    then the original text is returned.
+
+    Args:
+        text (str): The string to search within.
+        tag (str): The substring to search for in `text`.
+
+    Raises:
+        None
+
+    Returns:
+        str: A new string that includes all characters from the input string after
+             the first occurrence of the specified tag. If the tag is not found,
+             the original text is returned.
+    """
+
     t = text.find(tag)
     if t >= 0:
         text = text[t + len(tag) :]
--- source/A_GIS/Text/get_before_tag/__init__.py
+++ source/A_GIS/Text/get_before_tag/__init__.py
@@ -1,4 +1,22 @@
 def get_before_tag(*, text: str, tag: str):
+    """Extract substring before specified tag.
+
+    This function finds the first occurrence of a given tag within a provided text and returns
+    everything before it, including the tag itself. If the tag is not found in the text,
+    the original text is returned unchanged.
+
+    Args:
+        text (str): The input string to search for the tag in.
+        tag (str): The specific tag to find within the text.
+
+    Raises:
+        None
+
+    Returns:
+        str: The part of the original text that precedes the specified tag, or
+             the original text if the tag is not found.
+    """
+
     t = text.find(tag)
     if t >= 0:
         text = text[:t]
