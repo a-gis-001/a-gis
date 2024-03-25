7b3fa8c4737519a84947ffb3e2279857470e59f8
AEGIS 001 <a-gis-001@users.noreply.github.com>
2024-03-24 23:04:52 -0400

    Fix docstring

--- source/A_GIS/Text/find_and_replace/__init__.py
+++ source/A_GIS/Text/find_and_replace/__init__.py
@@ -1,15 +1,31 @@
 def find_and_replace(*, text: str, old: str, new: str, is_regex: bool = False):
-    """Find and replace within provided text
+    """Perform find-replace in text using flag for regex.
 
-    Can use plain strings or regular expressions.
-    Returns the total number of replacements made across all files.
+        This function searches for a specified string or regular
+        expression in the provided text and replaces it with
+        another string. The search can be performed as a plain
+        string match or using regular expressions, based on the
+        `is_regex` flag. It also counts the number of replacements
+        made.
 
-    Parameters:
-    - files: List of Path objects pointing to files where replacements should be made.
-    - old: The string or regular expression to find.
-    - new: The string to replace with.
-    - is_regex: If True, treat `old` as a regular expression. Otherwise, treat as plain string.
+    Args:
+        - text (str): The input text where replacements should
+            be made.
+        - old (str): The string or regular expression to find
+            and replace.
+        - new (str): The string to replace `old` with.
+        - is_regex (bool, optional): If True, treat `old` as a
+            regular expression. Otherwise, treat it as plain
+            string. Defaults to False.
+
+    Raises:
+        None
+
+    Returns:
+        Tuple[str, int]: A tuple containing the modified text and
+            the number of replacements made.
     """
+
     import re
 
     # Perform replacement and count occurrences
