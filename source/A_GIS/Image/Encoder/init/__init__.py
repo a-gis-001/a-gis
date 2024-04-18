ae8879c3a6ffc0e23242b921277ecfe6b4dd4b2e
AEGIS 001 <a-gis-001@users.noreply.github.com>
2024-04-17 22:04:22 -0400

    Update docstring

--- source/A_GIS/Image/Encoder/init/__init__.py
+++ source/A_GIS/Image/Encoder/init/__init__.py
@@ -1,26 +1,25 @@
 def init(*, model="clip-ViT-B-32"):
-    """Initialize an encoder for calculating image embeddings.
+    """Initialize a model for calculating image vector embeddings.
 
-    Currently this function initializes a sentence transformer model with the given name
-    and returns its instance and simply returns it. 
-    
-    Future versions of this function may return an `_Encoder` adapter class 
-    that has a method `encode` and provides different options than transformers.
+    Future versions may return an `_Encoder` adapter class that provides a method `encode`
+    and may offer additional functionality or options compared to the current approach.
 
     Args:
         model (str, optional):
-            The identifier of the pre-trained model to use. Defaults to
-            "clip-ViT-B-32". Supported models can be listed using `sentence_transformers.util.load_model_list()`.
+            The identifier of the pre-trained model to use for encoding images.
+            Defaults to "clip-ViT-B-32". Users can obtain a list of supported models by
+            calling `sentence_transformers.util.load_model_list()`.
 
     Returns:
-        SentenceTransformer:
-            A pre-trained model from the sentence transformers library,
-            ready to encode sentences into vector representations.
+        sentence_transformers.SentenceTransformer:
+            A pre-trained model from the
+            sentence transformers library, ready to encode sentences and potentially images
+            into vector representations.
 
     Raises:
         ValueError:
             If the specified model is not available in the sentence transformer's
-            pre-trained models list.
+            pre-trained models list or if the `model` argument is not a string.
     """
 
     import sentence_transformers
