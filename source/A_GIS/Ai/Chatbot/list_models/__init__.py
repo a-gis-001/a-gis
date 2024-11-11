fb21b54bb004923b669acadb5aa159b47c342ce9
AEGIS 001 <a-gis-001@users.noreply.github.com>
2024-11-10 20:19:20 -0500

    Update function docstring and improve model listing
    Updated the `list_models` function to provide a more accurate and
    detailed description of its purpose, parameters, return values, and
    behavior. Additionally, improved the logic for extracting model names
    from the OLLaMA library's response.

--- source/A_GIS/Ai/Chatbot/list_models/__init__.py
+++ source/A_GIS/Ai/Chatbot/list_models/__init__.py
@@ -1,5 +1,16 @@
 def list_models():
-    """List locally available models."""
+    """Return a list of installed OLLaMA model names.
+
+    This function interfaces with the OLLaMA library to obtain a list of
+    all available model names. It imports the OLLaMA module, retrieves
+    the list of models from the OLLaMA's `list` method, and then
+    extracts the model names from this list.
+
+    Returns:
+        :
+            list of str: A list containing the names of all installed
+            OLLaMA models.
+    """
     import ollama
 
     return [entry["model"] for entry in ollama.list()["models"]]
