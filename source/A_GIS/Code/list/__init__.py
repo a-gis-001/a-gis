f0e7967c0aba82ac8b16c829b0e3d08dc1d171ee
AEGIS 001 <a-gis-001@users.noreply.github.com>
2024-11-10 20:17:23 -0500

    Refactor list function to filter out non-functions
    Filter the output of the `list` function in A_GIS/Code/list/__init__.py
    to only include items identified as functions using
    A_GIS.Code.guess_type. This change adds a new parameter `functions_only`
    to control this behavior and updates the docstring accordingly.

--- source/A_GIS/Code/list/__init__.py
+++ source/A_GIS/Code/list/__init__.py
@@ -1,36 +1,41 @@
-def list(*, package_name="A_GIS", ignore=["_", "tests"]):
-    """List sub-packages within a specified package, excluding those with matching prefixes.
+def list(
+    *,
+    package_name="A_GIS",
+    ignore=["_", "tests"],
+    functions_only: bool = False,
+):
+    """Retrieve package modules and sub-packages with filtering.
 
-    This function retrieves and lists all sub-packages within a
-    specified package that are not excluded by the provided ignore. It
-    returns a structured representation of the package's contents,
-    including the paths to the modules or packages that were included
-    based on the ignore.
+    This function retrieves all modules and sub-packages from a given
+    Python package, as specified by `package_name`. It filters out any
+    modules or sub-packages that match the patterns in the `ignore`
+    list. Additionally, if `functions_only` is True, it will only
+    include items identified as functions using `A_GIS.Code.guess_type`.
+    The resulting data structure contains the full names and paths of
+    the included modules and packages.
 
     Args:
         package_name (str, optional):
-            The name of the package to list sub-packages from. Defaults
-            to "A_GIS".
-        ignore (list of str, optional):
-            A list of strings representing prefixes to include or
-            exclude when filtering modules and sub-packages. By default,
-            includes "_" and "tests".
+            The name of the package to list. Defaults to "A_GIS".
+        ignore (list, optional):
+            A list of patterns to exclude from the listing. Defaults to
+            ["_", "tests"].
+        functions_only (bool, optional):
+            If True, only include items that are identified as
+            functions. Defaults to False.
 
     Returns:
         dataclass:
             With the following attributes
 
-            - result (dict): A dictionary where keys are full names of
-              included packages/modules and values are their
-              corresponding file paths as strings.
+            - result (dict): A dictionary mapping full module names to
+              their paths.
             - package_name (str): The name of the package from which
-              the sub-packages were listed.
-            - ignore (list of str): The list of ignore used to
-              determine inclusion or exclusion of modules and sub-
-              packages.
-            - ignored (list of str): A list of full names of
-              packages/modules that were included based on the
-              ignore.
+              modules and sub-packages were listed.
+            - ignore (list): A list of patterns that were used to
+              exclude certain items.
+            - ignored (list): A list of full module names that were
+              excluded based on the `ignore` patterns.
     """
 
     import importlib
@@ -39,6 +44,8 @@ def list(*, package_name="A_GIS", ignore=["_", "tests"]):
     import sys
     import pkgutil
     import A_GIS.Code.make_struct
+    import A_GIS.Code.Unit.Name.to_path
+    import A_GIS.Code.guess_type
 
     root_package = importlib.import_module(package_name)
     package_path = root_package.__path__
@@ -58,6 +65,11 @@ def list(*, package_name="A_GIS", ignore=["_", "tests"]):
                 if x.startswith(filter):
                     ignored.append(full_name)
                     include = False
+        if (
+            functions_only
+            and A_GIS.Code.guess_type(file=pathlib.Path(path)) != "function"
+        ):
+            include = False
         if include:
             packages[full_name] = str(path)
 
